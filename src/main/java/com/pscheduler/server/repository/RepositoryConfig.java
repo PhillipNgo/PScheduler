942e14906ab2fee018e81b2ad77bdacd59cd4976
Phillip Ngo <ngophill@vt.edu>
2019-07-20 15:22:39 -0700

    Revert "Grade distribution parser (#32)" (#35)
    This reverts commit e337da8d3bd511a832b246876cdbf79ada5127a1.

--- src/main/java/com/pscheduler/server/repository/RepositoryConfig.java
+++ src/main/java/com/pscheduler/server/repository/RepositoryConfig.java
@@ -1,7 +1,6 @@
 package com.pscheduler.server.repository;
 
 import com.pscheduler.server.model.Course;
-import com.pscheduler.server.model.CourseGPA;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -11,6 +10,5 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Course.class);
-        config.exposeIdsFor(CourseGPA.class);
     }
 }
