3cc75554dfe8a5967ebd8351f3ba7f0035daa54b
Francis Nguyen <hoschn@gmail.com>
2019-07-21 16:21:53 -0400

    Removed Query Params

--- src/main/java/com/pscheduler/server/controller/CourseGPAController.java
+++ src/main/java/com/pscheduler/server/controller/CourseGPAController.java
@@ -36,17 +36,6 @@ public class CourseGPAController {
             @RequestParam(value="number", required = false) String number,
             @RequestParam(value="name", required = false) String name,
             @RequestParam(value="instructor", required = false) String instructor,
-            @RequestParam(value="crn", required = false) Integer crn,
-            @RequestParam(value="credits", required = false) Integer credits,
-            @RequestParam(value="gpa", required = false) Double gpa,
-            @RequestParam(value="students", required = false) Integer students,
-            @RequestParam(value="A", required = false) Double A,
-            @RequestParam(value="B", required = false) Double B,
-            @RequestParam(value="C", required = false) Double C,
-            @RequestParam(value="D", required = false) Double D,
-            @RequestParam(value="F", required = false) Double F,
-            @RequestParam(value="withdraws", required = false) Integer withdraws,
-            @RequestParam(value="term", required = false) Integer term,
             @RequestParam(value="sort", required = false) List<String> sorts,
             Pageable page
     ) {
@@ -56,15 +45,13 @@ public class CourseGPAController {
             List<CourseGPA> courses = query.stream().flatMap(q -> {
                 String searchTerm = q.toLowerCase().replaceAll(" ", "");
                 return courseRepo.searchAll(
-                        searchTerm, subject, number, name, instructor, crn,
-                        credits, gpa, students, A, B, C, D, F, withdraws, term, pageRequest
+                        searchTerm, subject, number, name, instructor, pageRequest
                 ).getContent().stream();
             }).collect(Collectors.toList());
             Page<CourseGPA> pagedCourses = new PageImpl<>(courses, pageRequest, courses.size());
             PagedResources<Resource<CourseGPA>> resources = assembler.toResource(pagedCourses);
             resources.add(linkTo(methodOn(CourseGPAController.class).getSearch(
-                    query, subject, number, name, instructor, crn, credits, gpa, students, A,
-                    B, C, D, F, withdraws, term, sorts, page
+                    query, subject, number, name, instructor, sorts, page
             )).withSelfRel());
             return ResponseEntity.ok(resources);
         } catch (Exception ex) {
--- src/main/java/com/pscheduler/server/repository/CourseGPARepository.java
+++ src/main/java/com/pscheduler/server/repository/CourseGPARepository.java
@@ -18,17 +18,6 @@ public interface CourseGPARepository extends PagingAndSortingRepository<CourseGP
         + " AND (:courseNumber IS NULL OR course.courseNumber = :courseNumber)"
         + " AND (:name IS NULL OR course.name = :name)"
         + " AND (:instructor IS NULL OR course.instructor = :instructor)"
-        + " AND (:crn IS NULL OR course.crn = :crn)"
-        + " AND (:credits IS NULL OR course.credits = :credits)"
-        + " AND (:gpa IS NULL OR course.gpa = :gpa)"
-        + " AND (:students IS NULL OR course.students = :students)"
-        + " AND (:A IS NULL OR course.A = :A)"
-        + " AND (:B IS NULL OR course.B = :B)"
-        + " AND (:C IS NULL OR course.C = :C)"
-        + " AND (:D IS NULL OR course.D = :D)"
-        + " AND (:F IS NULL OR course.F = :F)"
-        + " AND (:withdraws IS NULL OR course.withdraws = :withdraws)"
-        + " AND (:term IS NULL OR course.term = :term)"
     )
     @RestResource(exported = false)
     Page<CourseGPA> searchAll(
@@ -37,17 +26,6 @@ public interface CourseGPARepository extends PagingAndSortingRepository<CourseGP
             @Param("courseNumber") String courseNumber,
             @Param("name") String name,
             @Param("instructor") String instructor,
-            @Param("crn") Integer crn,
-            @Param("credits") Integer credits,
-            @Param("gpa") Double gpa,
-            @Param("students") Integer students,
-            @Param("A") Double A,
-            @Param("B") Double B,
-            @Param("C") Double C,
-            @Param("D") Double D,
-            @Param("F") Double F,
-            @Param("withdraws") Integer withdraws,
-            @Param("term") Integer term,
             Pageable page
     );
 }
