424932d7621e6e656ec4fd4c5be8d310cee28b6d
Phillip Ngo <ngophill@vt.edu>
2019-07-22 20:41:34 -0700

    Modulate generate.js, fix redirect query bug

--- src/main/js/utils/generate.js
+++ src/main/js/utils/generate.js
@@ -11,58 +11,63 @@ import ScheduleMaker from './ScheduleMaker';
 import 'whatwg-fetch';
 import { shortenUrl } from '../constants/resources';
 
+const genSchedules = (courseList, gap, sort) => {
+  let schedules = [];
+  if (!courseList.find(list => list.length === 0)) {
+    const scheduleMaker = new ScheduleMaker(courseList, parseInt(gap, 10));
+    try {
+      schedules = scheduleMaker.makeSchedules();
+      if (sort === 'yes') {
+        schedules.sort((schedule1, schedule2) => schedule2.gpa - schedule1.gpa);
+      }
+    } catch (e) {
+      schedules = e;
+    }
+  }
+  return schedules;
+};
+
+const fetchShortUrl = (courseList, formValues) => {
+  const queryObject = {
+    ...formValues,
+    c: courseList.map((course) => {
+      const name = `${course[0].subject}${course[0].courseNumber}`;
+      return JSON.stringify({
+        name,
+        ...(formValues.courses ? formValues.courses[`${name}${course.id}`] : {}),
+      });
+    }),
+  };
+  delete queryObject.courses;
+  return fetch(`${shortenUrl}?${stringify({ data: JSON.stringify(queryObject), prefix: 'generator' })}`)
+    .then(response => response.json());
+};
+
+const redirectToGenerator = (schedules, search = '') => startRedirect({
+  pathname: '/generator',
+  hash: schedules.length > 0 ? '#schedules' : '#timetable',
+  search: search.replace('==', '='),
+});
+
 const generateSchedules = (values, loadQuery) => (dispatch, getState) => {
   const { courseList } = getState().courses;
   if (courseList.length !== 0) {
     dispatch(startGenerating());
+
+    const shortCode = loadQuery ? Promise.resolve(loadQuery.substring(2))
+      : fetchShortUrl(courseList, values);
+
     addGPA(courseList).then((res) => {
       const filteredResults = filterCourses(values, res);
       dispatch(filteredCourses(filteredResults));
-      const filteredCourseList = filteredResults.map(result => (
-        result.filtered));
-      let schedules = [];
-      if (!filteredCourseList.find(list => list.length === 0)) {
-        const scheduleMaker = new ScheduleMaker(filteredCourseList, parseInt(values.gap, 10));
-        try {
-          schedules = scheduleMaker.makeSchedules();
-        } catch (e) {
-          schedules = e;
-        }
-        if (values.sortByGPA === 'yes') {
-          schedules.sort((schedule1, schedule2) => schedule2.gpa - schedule1.gpa);
-        }
-      }
-      const queryObject = {
-        ...values,
-        c: courseList.map((course) => {
-          const name = `${course[0].subject}${course[0].courseNumber}`;
-          return JSON.stringify({
-            name,
-            ...(values.courses ? values.courses[`${name}${course.id}`] : {}),
-          });
-        }),
-      };
-      delete queryObject.courses;
-      (loadQuery ? Promise.resolve() : fetch(`${shortenUrl}?${stringify({ data: JSON.stringify(queryObject), prefix: 'generator' })}`))
-        .then(response => (loadQuery ? loadQuery.substring(2) : response.json()))
-        .then((result) => {
-          const querystring = `q=${result}`;
-          dispatch(startRedirect({
-            pathname: '/generator',
-            hash: schedules.length > 0 ? '#schedules' : '#timetable',
-            search: result ? querystring : '',
-          }));
-        })
-        .catch(() => {
-          dispatch(startRedirect({
-            pathname: '/generator',
-            hash: schedules.length > 0 ? '#schedules' : '#timetable',
-            search: '',
-          }));
-        })
-        .then(() => {
-          dispatch(endGenerating(schedules));
-        });
+      const filteredCourseList = filteredResults.map(result => result.filtered);
+
+      const schedules = genSchedules(filteredCourseList, values.gap, values.sortByGPA);
+
+      shortCode
+        .then(code => dispatch(redirectToGenerator(schedules, code ? `q=${code}` : '')))
+        .catch(() => dispatch(redirectToGenerator(schedules)))
+        .then(() => dispatch(endGenerating(schedules)));
     });
   }
 };
