cc4ebf4e8d433d047a74688a8600883856cc49ba
Phillip Ngo <ngophill@vt.edu>
2019-08-18 11:57:24 -0700

    Styled filter util

--- src/main/js/utils/filter.js
+++ src/main/js/utils/filter.js
@@ -30,27 +30,38 @@ const filterTime = (start, end, course) => {
 
 const filterCourses = (values, courseList) => {
   const filteredCourseList = [];
+
   courseList.forEach((list) => {
     const currList = {
       name: `${list[0].subject} ${list[0].courseNumber}`,
     };
     filteredCourseList.push(currList);
     currList.removed = [];
+
     let filtered = list.filter((course) => {
-      const keep = filterFreeDays(values.free, course)
-        && filterTime(`${values.h1}:${values.m1}${values.start}`, `${values.h2}:${values.m2}${values.end}`, course);
+      const keep = (
+        filterFreeDays(values.free, course)
+        && filterTime(
+          `${values.h1}:${values.m1}${values.start}`,
+          `${values.h2}:${values.m2}${values.end}`,
+          course,
+        )
+      );
       if (!keep) {
         currList.removed.push(course);
       }
       return keep;
     });
+
     const key = `${list[0].subject}${list[0].courseNumber}${list.id}`;
     if (values.courses && values.courses[key]) {
       const filters = values.courses[key];
       filtered = filtered.filter((course) => {
-        const keep = filterCrns(filters.crns, course)
+        const keep = (
+          filterCrns(filters.crns, course)
           && filterTypes(filters.types, course)
-          && filterInstructors(filters.instructors, course);
+          && filterInstructors(filters.instructors, course)
+        );
         if (!keep) {
           currList.removed.push(course);
         }
@@ -59,6 +70,7 @@ const filterCourses = (values, courseList) => {
     }
     currList.filtered = filtered;
   });
+
   return filteredCourseList;
 };
 
